5f302768198e26208de6894be6c038e0111bea41
Mohsen Koohi <koohi@burge.ir>
2016-09-28 17:59:43 +0330

    rewards -> customer -> submit prize

--- ATS/CMF/Web/application/language/en/ce_reward_lang.php
+++ ATS/CMF/Web/application/language/en/ce_reward_lang.php
@@ -9,3 +9,15 @@ $lang['reward_value']="Value";
 $lang['more_description']="More description";
 $lang['initial_reward_value']="Initial reward";
 $lang['rewards_added_successfully']="Rewards added successfully.";
+
+$lang['rewards_list']="Rewards list";
+$lang['value']="Rewards ";
+$lang['name']="Name";
+$lang['description']="Description";
+$lang['reward']="Reward";
+$lang['total']="Total";
+
+$lang['submit_prize']="Submit Prize";
+$lang['receiving_prize']="Prize";
+$lang['total_rewards']="Total";
+$lang['use_all']="Use all rewards";
