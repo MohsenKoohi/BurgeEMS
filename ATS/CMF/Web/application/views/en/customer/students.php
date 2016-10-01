a45ad83374520069ab094bda5b1d594a9669e5f5
Mohsen Koohi <koohi@burge.ir>
2016-10-01 08:11:10 +0330

    style correction

--- ATS/CMF/Web/application/views/en/customer/students.php
+++ ATS/CMF/Web/application/views/en/customer/students.php
@@ -17,13 +17,10 @@
 		</div>
 		<?php foreach($students as $st) { ?>
 			<div class="row even-odd-bg">		
-				<div class="two columns">
-					&nbsp;
+				<div class="tweleve columns">
 					<?php if($st['customer_image_hash']) { ?>
 						<img src="<?php echo get_customer_image_url($st['customer_id'],$st['customer_image_hash']);?>"/>
 					<?php } ?>
-				</div>
-				<div class="six columns">
 					<span><?php echo $st['customer_name'];?></span>
 				</div>
 			</div>
--- ATS/CMF/Web/application/views/en/customer/teachers.php
+++ ATS/CMF/Web/application/views/en/customer/teachers.php
@@ -3,11 +3,17 @@
 		<h1>{teachers_text}</h1>			
 		<?php foreach($teachers as $tc) { ?>
 			<div class="row even-odd-bg">		
-				<div class="three columns">
+				<div class="four columns">
 					<span><?php echo $tc['customer_name'];?></span>
 				</div>
-				<div class="six columns">
+				<div class="four columns">
 					<span><?php echo $tc['customer_subject'];?></span>
+				</div>
+				<div class="four columns">
+					<?php if($tc['customer_address']) { ?>
+						<label>{contact_time_text}</label>
+						<span><?php echo $tc['customer_address'];?></span>
+					<?php } ?>
 				</div>
 			</div>
 		<?php } ?>
