33b50f56c8dd3283010a5bb07a94e410339e785e
Mohsen Koohi <koohi@burge.ir>
2016-10-14 01:20:36 +0330

    question collector -> admin -> english translation

---
+++ ATS/CMF/Web/application/language/en/ae_question_collection_lang.php
@@ -0,0 +1,25 @@
+<?php 
+
+$lang['questions_collection']="Questions Collection";
+$lang['questions_list']="List";
+$lang['add_question']="Add";
+
+$lang['grade']="Grade";
+$lang['course']="Course";
+$lang['subject']="Subject";
+$lang['add_file']="File";
+$lang['select_file']="Select";
+$lang['file_subject']="Title";
+$lang['please_fill_all_fields']="Please fill all fields";
+$lang['the_new_question_collection_added_successfully']="The new questions added successfully";
+$lang['view']="View";
+$lang['are_you_sure_to_submit_the_new_questions_set']="Are you sure to submit this new question set?";
+
+$lang['registrar']="Registrar";
+$lang['date']="Date";
+$lang['file']="File";
+$lang['delete']="Delete";
+$lang['are_you_sure_to_delete_this_collection']="Are you sure to delete?";
+$lang['question_set_deleted_successfully']="The question set successfully.";
+$lang['']="";
+
