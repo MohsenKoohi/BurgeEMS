3d6c3fd6e6766cb8d6403ca4002b91f7d424e3b1
Mohsen Koohi <koohi@burge.ir>
2016-10-18 10:38:45 +0330

    style correction

--- ATS/CMF/Web/application/views/en/customer/reward_teacher_values.php
+++ ATS/CMF/Web/application/views/en/customer/reward_teacher_values.php
@@ -2,19 +2,19 @@
 	<div class="container student-rewards-list">
 		<h1>{rewards_text}</h1>
 		<h2>{reward_class_name}{comma_text} {reward_subject}</h2>			
+		<div class="anti-float">
+			<span class="date"><b>{reward_date}</b></span>
+		</div>
+		<br><br>
 		<?php if($reward_editable) { ?>
 			<div class="row general-buttons" style="margin-bottom:50px;">
 				<div class="anti-float two columns button button-type2" onclick="document.location='{edit_link}';">
 					{edit_text}
 				</div>
 			</div>
-		<?php } ?>
-
-		<div class="anti-float">
-			<span class="date"><b>{reward_date}</b></span>
-		</div>
-		
-		<div class="row" style="margin-top:50px;">
+			<br><br>
+		<?php } ?>		
+		<div class="row">
 			<?php 
 			 	$reward_date=explode(" ",$reward_date)[0];
 				foreach($students_rewards as $st) { 
