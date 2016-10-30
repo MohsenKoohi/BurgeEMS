39352425fd1310ef56c2e4d956a06afc0bdb2a32
Mohsen Koohi <koohi@burge.ir>
2016-10-30 20:58:33 +0330

    message manager module -> customer -> english

--- ATS/CMF/Web/application/language/en/ce_message_lang.php
+++ ATS/CMF/Web/application/language/en/ce_message_lang.php
@@ -1,25 +1,15 @@
 <?php 
+$lang['group_1_name']="Teachers and parents community";
 
-$lang['department_customers']="Customers";
-$lang['department_agents']="Agents";
-$lang['department_management']="Management";
-
-$lang['department']="Department";
 $lang['subject']="Subject";
 $lang['content']="Content";
 $lang['submit']="Submit";
 
 $lang['fill_all_fields']="Please fill all fields.";
 $lang['captcha_incorrect']="Captcha is incorrect.";
-$lang['department_message_sent_successfully']="We have received your message and contact you ASAP.";
-
-$lang['to_send_message_you_should_login']="You should login to your account before sending a message:";
-$lang['login']="Login";
-$lang['register']="Signup";
 
 $lang['sender_from']="From";
 $lang['receiver_to']="To";
-$lang['content_of_last_message']="Content";
 $lang['view_details']="View";
 
 $lang['message']="Message";
@@ -30,4 +20,14 @@ $lang['reply_message_sent_successfully']="Your reply sent successfully.";
 
 $lang['send_message']="Send Message";
 $lang['receiver']="Receiver";
-$lang['message_sent_successfully']="Your message sent successfully.";
+$lang['message_sent_successfully']="Your message have been sent successfully.";
+$lang['date']="Date";
+$lang['reply_to']="Reply to";
+
+$lang['class']="Class";
+$lang['student']="Student";
+$lang["receiver_has_not_selected"]="Receiver hasn't been selected";
+$lang['are_you_sure_to_submit']="Are you sure?";
+$lang['it_is_not_possible_to_send_message_to_this_class']="It's not possible to send message to this class";
+$lang['it_is_not_possible_to_send_message_to_these_students']="It's not possible to send message to these students";
+$lang['messages_sent_successfully']="Message(s) have been sent successfully.";
