b9c265d725db6483cf0410f8c66a1642db066d73
Mohsen Koohi <koohi@burge.ir>
2016-11-01 23:42:23 +0330

    dashboard -> parent messages

--- ATS/CMF/Web/application/views/en/customer/dashboard_parent.php
+++ ATS/CMF/Web/application/views/en/customer/dashboard_parent.php
@@ -2,15 +2,13 @@
 	<div class="container dashboard">
 		<h4>{customer_name}</h4>
 		<ul class="dash-ul">
-			<?php if($groups){ ?>
-				<li>
-					<a>{messages_text}</a>
-					<ul class='dash-ul'>
-						<li ><a href="<?php echo get_link('customer_message');?>">{message_inbox_text}</a></li>
-						<li><a href="<?php echo get_link('customer_message_send');?>">{send_message_text}</a></li>
-					</ul>
-				</li>
-			<?php } ?>
+			<li>
+				<a>{messages_text}</a>
+				<ul class='dash-ul'>
+					<li ><a href="<?php echo get_link('customer_message');?>">{message_inbox_text}</a></li>
+					<li><a href="<?php echo get_link('customer_message_send');?>">{send_message_text}</a></li>
+				</ul>
+			</li>
 
 			<li>
 				<a href="<?php echo get_link('customer_logout');?>">
